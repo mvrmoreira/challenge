75a15fa5a2771054d0f626af6b33b725edbbc126
Felipe Vieira <felipe.tio@gmail.com>
2018-04-13 12:10:31 -0300

    changed test and added missing files

--- test/controllers/api/restaurants_controller_test.rb
+++ test/controllers/api/restaurants_controller_test.rb
@@ -4,32 +4,17 @@ class Api::RestaurantsControllerTest < ActionDispatch::IntegrationTest
   include FactoryBot::Syntax::Methods
 
   setup do
+    # criantdo para todos os testes 5 restaurantes
+    # em 2 culinarias
     create(:culinary, name:"Italiana", restaurant_count: 3)
     create(:culinary, name:"Japonesa", restaurant_count: 2)
   end
 
-  test "list all restaurants" do
-    get "/api/restaurants"
-
-    assert_response :success
-    assert_equal(5, JSON.parse(@response.body).count)
-  end
-
-  test "filter japanese restaurants" do
-    culinary = Culinary.find_by_name "Japonesa"
-
-    get "/api/restaurants", params:{culinaryId: culinary.id}
-
-    assert_response :success
-    assert_equal(2, JSON.parse(@response.body).count)
-  end
-
   test "filter restaurants for available for reservation today" do
     # hoje esse restaurante aceita no maximo 10 pessoas
-    # porem já exite 3 reservas de 2 pessoas criadas
+    # porem já exite 3 reservas de 2 pessoas criadas cada
     restaurant = Restaurant.first
     now = DateTime.now
-
     3.times{ create(:reservation, restaurant: restaurant, event_time: now, seats: 2) }
 
     get "/api/restaurants", params: { date: now, seats: 2 }
@@ -47,7 +32,7 @@ class Api::RestaurantsControllerTest < ActionDispatch::IntegrationTest
 
   test "filter restaurants for available for reservation tomorow" do
     # amanhã esse restaurante aceita no maximo 4 pessoas
-    # porem já exite uma reservas de 2 pessoas criadas
+    # porem já exite uma reservas de 2 pessoas criadas cada
     restaurant = Restaurant.first
     tomorrow =  DateTime.tomorrow
     create(:reservation, restaurant: restaurant, event_time: tomorrow, seats: 2)
@@ -63,5 +48,20 @@ class Api::RestaurantsControllerTest < ActionDispatch::IntegrationTest
     get "/api/restaurants", params: { date: tomorrow, seats: 6 }
     assert_response :success
     assert_equal(4, JSON.parse(@response.body).count)
+  end
+  test "list all restaurants" do
+    get "/api/restaurants"
+
+    assert_response :success
+    assert_equal(5, JSON.parse(@response.body).count)
+  end
+
+  test "filter japanese restaurants" do
+    culinary = Culinary.find_by_name "Japonesa"
+
+    get "/api/restaurants", params:{culinaryId: culinary.id}
+
+    assert_response :success
+    assert_equal(2, JSON.parse(@response.body).count)
   end
 end
