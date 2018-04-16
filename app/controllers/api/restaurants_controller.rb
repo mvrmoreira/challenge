9c24563958af7d6d2e0392e0f760004b5aea2c28
Matheus Moreira <mvrmoreira@gmail.com>
2018-04-16 12:09:47 -0300

    Add filters to model

--- app/controllers/api/restaurants_controller.rb
+++ app/controllers/api/restaurants_controller.rb
@@ -1,17 +1,18 @@
 class Api::RestaurantsController < ApplicationController
   def index
-    @results = Restaurant.all
 
     if params[:culinaryId]
-      @results = filter(@results)
+      @results = Restaurant.by_culinary(params[:culinaryId])
+    else
+      @results = Restaurant.all
     end
 
     render json: @results
   end
 
-  def filter(restaurants)
-      restaurants.select do |restaurant|
-        restaurant.culinary_ids.include?(params[:culinaryId].to_i)
-      end
-  end
+  private
+
+    def restaurant_params
+      params.permit(:culinaryId)
+    end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,7 @@
 class Restaurant < ApplicationRecord
   has_and_belongs_to_many :culinaries, dependent: :destroy
+
+  scope :by_culinary, -> (culinaryId) {
+  	joins(:culinaries).where("culinary_id = ?", culinaryId)
+  } 
 end
